a164dbececa5af3346a5d44afb2d600535773de0
parth022002 <parthahuja021102@gmail.com>
2024-02-21 18:43:00 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/*.html"],
+  theme: {
+    extend: {
+      colors:{
+        chatblack: {50: '#2c2c2c'},
+      
+      }
+    },
+  },
+  plugins: [],
+}
+
